9f3f340373249b56c3cf3a2271232d9581cac4cf
inpyoianlee <65551238+inpyoianlee@users.noreply.github.com>
2021-11-05 11:22:41 -0700

    sorry one more thing

--- src/components/Create_Routines_Post.js
+++ src/components/Create_Routines_Post.js
@@ -1,84 +1,84 @@
-// import React, { useState } from 'react';
-// import { createRoutine } from '../api';
-// import { getToken } from '../auth';
+import React, { useState } from 'react';
+import { createRoutine } from '../api';
+import { getToken } from '../auth';
 
-// const createRoutine = ({ id, creatorId, isPublic, name, goal, token }) => {
-//     const [id, setid] = useState('');
-//     const [creatorId, setcreatorId] = useState('');
-//     const [name, setname] = useState('');
-//     const [goal, setgoal] = useState('');
-//     const [isPublic, setisPublic] = useState(false);
-//     const [SaveRoutine, setSaveRoutine] = useState(false);
+const createRoutine = ({ id, creatorId, isPublic, name, goal, token }) => {
+    const [id, setid] = useState('');
+    const [creatorId, setcreatorId] = useState('');
+    const [name, setname] = useState('');
+    const [goal, setgoal] = useState('');
+    const [isPublic, setisPublic] = useState(false);
+    const [SaveRoutine, setSaveRoutine] = useState(false);
 
-//     return (
-//         <div className="create-post">
-//             <h2>Add a New Routine</h2>
-//             <form
-//                 className="create-post-form"
-//                 onSubmit={async (e) => {
-//                     e.preventDefault();
-//                     const userToken = getToken();
-//                     try {
-//                         !id ? alert('All ids are required. Please enter your id') : null;
-//                         !creatorId ? alert('All creatorIds are required. Please enter your creator id') : null;
-//                         !name ? alert('Naming Activities are required. Please enter the name of Routine') : null;
-//                         !goal ? alert('Please enter a goal') : null;
+    return (
+        <div className="create-post">
+            <h2>Add a New Routine</h2>
+            <form
+                className="create-post-form"
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    const userToken = getToken();
+                    try {
+                        !id ? alert('All ids are required. Please enter your id') : null;
+                        !creatorId ? alert('All creatorIds are required. Please enter your creator id') : null;
+                        !name ? alert('Naming Activities are required. Please enter the name of Routine') : null;
+                        !goal ? alert('Please enter a goal') : null;
                 
-//                         const results = await createPost(id, name, goal, isPublic, SaveRoutine, userToken);
-//                         setid('');
-//                         setcreatorId('');
-//                         setname('');
-//                         setgoal('');
-//                         setisPublic(false);
-//                         setSaveRoutine(false);
+                        const results = await createPost(id, name, goal, isPublic, SaveRoutine, userToken);
+                        setid('');
+                        setcreatorId('');
+                        setname('');
+                        setgoal('');
+                        setisPublic(false);
+                        setSaveRoutine(false);
 
-//                         const allPostsCopy = allPosts.slice();
-//                         allPostsCopy.unshift(results.data.post);
-//                         setAllPosts(allPostsCopy);
-//                         const newUserPost = allPostsCopy.filter(e => {
-//                             if (e.author._id === userId) {
-//                                 return e
-//                             }
-//                         });
-//                         setUserPosts(newUserPost);
-//                     } catch (err) {
-//                         console.log(err);
-//                     } finally {
+                        const allPostsCopy = allPosts.slice();
+                        allPostsCopy.unshift(results.data.post);
+                        setAllPosts(allPostsCopy);
+                        const newUserPost = allPostsCopy.filter(e => {
+                            if (e.author._id === userId) {
+                                return e
+                            }
+                        });
+                        setUserPosts(newUserPost);
+                    } catch (err) {
+                        console.log(err);
+                    } finally {
 
-//                     }
-//                 }}>
+                    }
+                }}>
 
-//                 <input
-//                     type="text"
-//                     id="Id"
-//                     value={title}
-//                     onChange={(e) => setid(e.target.value)}
-//                     placeholder="Id" />
-//                       <input
-//                     type="text"
-//                     id="creatorId"
-//                     value={title}
-//                     onChange={(e) => setcreatorId(e.target.value)}
-//                     placeholder="Id" />
-//                 <input
-//                     type="text"
-//                     id="routine-name"
-//                     value={name}
-//                     onChange={(e) => setname(e.target.value)}
-//                     placeholder="Name" />
-//                 <label className="post-checkbox">
-//                     <input
-//                         type="checkbox"
-//                         id="save-routine"
-//                         value={SaveRoutine}
-//                         onChange={(e) => setSaveRoutine(true)}
-//                     />
-//                     <p>Willing to Save?</p>
-//                 </label>
-//                 <button>Create Routine Post</button>
-//             </form>
-//         </div>
-//     )
-// }
+                <input
+                    type="text"
+                    id="Id"
+                    value={title}
+                    onChange={(e) => setid(e.target.value)}
+                    placeholder="Id" />
+                      <input
+                    type="text"
+                    id="creatorId"
+                    value={title}
+                    onChange={(e) => setcreatorId(e.target.value)}
+                    placeholder="Id" />
+                <input
+                    type="text"
+                    id="routine-name"
+                    value={name}
+                    onChange={(e) => setname(e.target.value)}
+                    placeholder="Name" />
+                <label className="post-checkbox">
+                    <input
+                        type="checkbox"
+                        id="save-routine"
+                        value={SaveRoutine}
+                        onChange={(e) => setSaveRoutine(true)}
+                    />
+                    <p>Willing to Save?</p>
+                </label>
+                <button>Create Routine Post</button>
+            </form>
+        </div>
+    )
+}
 
-// export default createRoutine;
+export default createRoutine;
